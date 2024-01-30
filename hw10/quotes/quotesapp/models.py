526d084ff22baef64a03697f97f5476a384d0fea
olga-ivanko <o.ivanko@me.com>
2024-01-30 11:38:07 +0200

    first commit

---
+++ hw10/quotes/quotesapp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    fullname = models.CharField(null=False, blank=False)
+    born_date = models.DateField(null=False, blank=False)
+    born_location = models.CharField(null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f"{self.fullname}"
+
+
+class Tag(models.Model):
+    name = models.CharField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Quote(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    quote = models.TextField(null=False, blank=False)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return f"{self.quote}"
