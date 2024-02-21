95da92388de8ac3d0b840add99aaf67585e42a21
olga-ivanko <o.ivanko@me.com>
2024-02-22 00:34:50 +0200

    password reset added

--- hw10/quotes/quotes/settings.py
+++ hw10/quotes/quotes/settings.py
@@ -122,8 +122,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.meta.ua"
+EMAIL_PORT = 465
+EMAIL_STARTTLS = False
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = "o.v.ivanko@meta.ua"
+EMAIL_HOST_PASSWORD = "647735_Gg"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
